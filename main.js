1bac0f175d48e609fda4fc93b668c51d71aeef3c
Alexp-idk1011 <83146847+Alexp-idk1011@users.noreply.github.com>
2022-07-31 19:50:38 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var noseX = 0
-var noseY = 0
+var featureX = 0
+var featureY = 0
 
 function preload() {
+    feature = loadImage("mustache.png");
 
 }
 
@@ -12,13 +13,27 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
 }
 
 function draw() {
     image(video, 0, 0, 400, 300);
-
+    image(feature, featureX, featureY, 30, 30);
 }    
 
 function take_snapshot() {
     save('My_filter_img');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        featureX = results[0].pose.nose.x + 40;
+        featureY = results[0].pose.nose.y;
+    }
+}
+
+function modelLoaded() {
+    console.log("poseNet is initialized")
 }
